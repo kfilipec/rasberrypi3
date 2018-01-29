83ca04f9bf3678236ecef4a55a2177e8d7ebcf17
kfilipec <kf181004@gmail.com>
2018-01-29 12:30:30 +0100

    Add files via upload

---
+++ switchInput.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <wiringPi.h>
+using namespace std;
+
+int switchState;
+
+#define GREEN 18
+#define RED 15
+#define switchInput 14
+
+int main(){
+	
+	wiringPiSetupGpio();
+	pinMode(GREEN, OUTPUT);
+	pinMode(RED, OUTPUT);
+	pinMode(switchInput, INPUT);
+	
+	
+	while(1){
+		switchState= digitalRead(switchInput);
+		if(switchState==0) {
+			digitalWrite(GREEN, LOW);
+			digitalWrite(RED, HIGH);
+			delay(500);
+			digitalWrite(RED, LOW);
+			delay(500);
+		}
+		else if(switchState==1) {
+			digitalWrite(RED, LOW);
+			digitalWrite(GREEN, HIGH);
+			delay(500);
+			digitalWrite(GREEN, LOW);
+			delay(500);
+		}
+		else {
+			digitalWrite(RED, HIGH);
+			digitalWrite(GREEN, HIGH);
+		}
+	}
+}
